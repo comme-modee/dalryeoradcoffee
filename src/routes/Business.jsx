76526b5d6240dc5736aa5ece57673f8141ed3346
modee <modee@modeeui-MacBookAir.local>
2025-07-10 14:55:49 +0900

    edit ppt

--- src/routes/Business.jsx
+++ src/routes/Business.jsx
@@ -12,34 +12,32 @@ return (
     <div className='container-business'>
         <div className={`intro-img ${activeTab === '브리딩커피바' ? 'intro-img-breathing' : 'intro-img-walden'}`}></div>
         <div className='content'>
-            <div className='item item-1'>
-                <div className='title-container'>
-                    <div
-                        className={`title ${activeTab === '브리딩커피바' ? 'active' : ''}`}
-                        onClick={() => setActiveTab('브리딩커피바')}
-                    >
-                        브리딩커피바 소셜프랜차이즈 사업
-                    </div>
-                    <div
-                        className={`title ${activeTab === '월든' ? 'active' : ''}`}
-                        onClick={() => setActiveTab('월든')}
-                    >
-                        월든 청년숲학교
-                    </div>
+            <div className='title-container'>
+                <div
+                    className={`title ${activeTab === '브리딩커피바' ? 'active' : ''}`}
+                    onClick={() => setActiveTab('브리딩커피바')}
+                >
+                    브리딩커피바 소셜프랜차이즈 사업
                 </div>
-                <div className='img-container'>
-                    {activeTab === '브리딩커피바' && (
-                    <>
-                        <img src={businessBreathingImage1} alt='브리딩 이미지1' />
-                        <img src={businessBreathingImage2} alt='브리딩 이미지2' />
-                    </>
-                    )}
-                    {activeTab === '월든' && (
-                    <>
-                        <img src={businessWaldenImage1} alt='월든 이미지' />
-                    </>
-                    )}
+                <div
+                    className={`title ${activeTab === '월든' ? 'active' : ''}`}
+                    onClick={() => setActiveTab('월든')}
+                >
+                    월든 청년숲학교
                 </div>
+            </div>
+            <div className='img-container'>
+                {activeTab === '브리딩커피바' && (
+                <>
+                    <img src={businessBreathingImage1} alt='브리딩 이미지1' />
+                    <img src={businessBreathingImage2} alt='브리딩 이미지2' />
+                </>
+                )}
+                {activeTab === '월든' && (
+                <>
+                    <img src={businessWaldenImage1} alt='월든 이미지' />
+                </>
+                )}
             </div>
         </div>
     </div>
