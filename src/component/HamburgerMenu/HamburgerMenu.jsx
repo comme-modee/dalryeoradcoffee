34556e9dbafafcffb19660ec6c1a078b20a50a47
modee <modee@modeeui-MacBookAir.local>
2025-07-10 18:35:01 +0900

    edit header

--- src/component/HamburgerMenu/HamburgerMenu.jsx
+++ src/component/HamburgerMenu/HamburgerMenu.jsx
@@ -38,12 +38,17 @@ const HamburgerMenu = () => {
       </div>
 
       <nav className={`hamburger-dropdown ${open ? 'open' : ''}`}>
-        <p className='menu-item'>달려라커피</p>
-        <Link to='/about' className='submenu' onClick={() => setOpen(false)}>회사 소개</Link>
+        {/* <p className='menu-item'>달려라커피</p> */}
+        <Link to='/about' className='menu-item' onClick={() => setOpen(false)}>회사 소개</Link>
+        <Link to='/ceo' className='menu-item' onClick={() => setOpen(false)}>대표 경력 및 성과</Link>
+        <Link to='/business' className='menu-item' onClick={() => setOpen(false)}>사업 소개</Link>
+        <Link to='/service' className='menu-item' onClick={() => setOpen(false)}>서비스 소개</Link>
+        <Link to='/menu' className='menu-item' onClick={() => setOpen(false)}>메뉴 안내</Link>
+        {/* <Link to='/about' className='submenu' onClick={() => setOpen(false)}>회사 소개</Link>
         <Link to='/ceo' className='submenu' onClick={() => setOpen(false)}>대표 경력 및 성과</Link>
         <Link to='/business' className='submenu' onClick={() => setOpen(false)}>사업 소개</Link>
         <Link to='/service' className='submenu' onClick={() => setOpen(false)}>서비스 소개</Link>
-        <Link to='/menu' className='submenu' onClick={() => setOpen(false)}>메뉴 안내</Link>
+        <Link to='/menu' className='submenu' onClick={() => setOpen(false)}>메뉴 안내</Link> */}
         <Link to='/momostable' className='menu-item' onClick={() => setOpen(false)}>모모스테이블</Link>
         <Link to='/roza119' className='menu-item' onClick={() => setOpen(false)}>로자119</Link>
         {/* <Link to='/breathbeside' className='menu-item' onClick={() => setOpen(false)}>쉼곁숨 프로젝트</Link> */}
--- src/component/Header/Header.jsx
+++ src/component/Header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
         <div className={`header ${pathname === '/' ? 'header-main-page' : 'header-other-page'}`}>
             <Link to='/' className='logo'></Link>
             <div className='menu'>
-                <div className='menu-item'>
+                {/* <div className='menu-item'>
                     <div className='dropdown-trigger'>달려라커피</div>
                     <div className='dropdown'>
                         <Link to='/about'>회사 소개</Link>
@@ -19,27 +19,27 @@ const Header = () => {
                         <Link to='/service'>서비스 소개</Link>
                         <Link to='/menu'>메뉴 소개</Link>
                     </div>
+                </div> */}
+                <div className='menu-item'>
+                    <Link to='/about' className='dropdown-trigger'>회사소개</Link>
+                </div>
+                <div className='menu-item'>
+                    <Link to='/ceo' className='dropdown-trigger'>대표 경력 및 성과</Link>
+                </div>
+                <div className='menu-item'>
+                    <Link to='/business' className='dropdown-trigger'>사업소개</Link>
+                </div>
+                <div className='menu-item'>
+                    <Link to='/service' className='dropdown-trigger'>서비스소개</Link>
+                </div>
+                <div className='menu-item'>
+                    <Link to='/menu' className='dropdown-trigger'>메뉴소개</Link>
                 </div>
                 <div className='menu-item'>
                     <Link to='/momostable' className='dropdown-trigger'>모모스테이블</Link>
-                    {/* <div className='dropdown'>
-                        <Link to='/momostable/menu'>메뉴</Link>
-                        <Link to='/momostable/story'>스토리</Link>
-                    </div> */}
                 </div>
                 <div className='menu-item'>
                     <Link to='roza119' className='dropdown-trigger'>로자119</Link>
-                    {/* <div className='dropdown'>
-                        <Link to='/roza119/intro'>소개</Link>
-                        <Link to='/roza119/contact'>문의</Link>
-                    </div> */}
-                </div>
-                <div className='menu-item'>
-                    {/* <Link to='breathbeside' className='dropdown-trigger'>쉼곁숨 프로젝트</Link> */}
-                    {/* <div className='dropdown'>
-                        <Link to='/breathbeside/about'>소개</Link>
-                        <Link to='/breathbeside/join'>참여하기</Link>
-                    </div> */}
                 </div>
             </div>
             <div className='mobile'>
